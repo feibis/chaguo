23a1877f1ebffd045272a8c020254257d30c4520
Piotr Kulpinski <piotr@kulpinski.pl>
2025-02-21 13:51:14 +0100

    improve the media assets to include timestamps for clearing cache

--- components/common/link.tsx
+++ components/common/link.tsx
@@ -3,6 +3,10 @@
 import NextLink from "next/link"
 import { type ComponentProps, useState } from "react"
 
+/**
+ * This component is used to create a link that prefetches the page when the user hovers over it.
+ * If you're ok with the default behavior of NextLink, just use "next/link" instead.
+ */
 export const Link = ({ prefetch: nextPrefetch, ...props }: ComponentProps<typeof NextLink>) => {
   const [prefetch, setPrefetch] = useState(nextPrefetch ?? false)
 
