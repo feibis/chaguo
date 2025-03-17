e69afb12d59ccb1704c54ca01b9fc17727beb7bd
Piotr Kulpinski <piotr@kulpinski.pl>
2025-03-17 13:57:46 +0100

    update tool action buttons

--- components/web/tools/tool-actions.tsx
+++ components/web/tools/tool-actions.tsx
@@ -1,21 +1,14 @@
 "use client"
 
-import { EllipsisIcon, ShieldPlusIcon, SparklesIcon, TriangleAlertIcon } from "lucide-react"
+import { BadgeCheckIcon, SparklesIcon, TriangleAlertIcon } from "lucide-react"
 import Link from "next/link"
 import type { ComponentProps } from "react"
 import { useState } from "react"
 import { Button } from "~/components/common/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "~/components/common/dropdown-menu"
 import { Stack } from "~/components/common/stack"
-import { TooltipProvider } from "~/components/common/tooltip"
+import { Tooltip, TooltipProvider } from "~/components/common/tooltip"
 import { ToolClaimDialog } from "~/components/web/dialogs/tool-claim-dialog"
 import { ToolReportDialog } from "~/components/web/dialogs/tool-report-dialog"
-import { navLinkVariants } from "~/components/web/ui/nav-link"
 import { useSession } from "~/lib/auth-client"
 import type { ToolOne } from "~/server/web/tools/payloads"
 import { cx } from "~/utils/cva"
@@ -32,49 +25,48 @@ export const ToolActions = ({ tool, children, className, ...props }: ToolActions
   return (
     <TooltipProvider delayDuration={250}>
       <Stack size="sm" wrap={false} className={cx("justify-end", className)} {...props}>
-        {!tool.isFeatured && tool.owner && tool.owner?.email === session?.user.email && (
-          <Button variant="secondary" prefix={<SparklesIcon className="text-yellow-500" />} asChild>
+        {!tool.isFeatured && tool.owner && tool.owner.id === session?.user.id && (
+          <Button
+            size="md"
+            variant="secondary"
+            prefix={<SparklesIcon className="text-inherit" />}
+            className="text-blue-600 dark:text-blue-400"
+            asChild
+          >
             <Link href={`/submit/${tool.slug}`}>Promote</Link>
           </Button>
         )}
 
         {!tool.owner && (
           <Button
+            size="md"
             variant="secondary"
-            prefix={<ShieldPlusIcon className="text-yellow-500" />}
+            prefix={<BadgeCheckIcon className="text-inherit" />}
             onClick={() => setIsClaimOpen(true)}
+            className="text-blue-600 dark:text-blue-400"
           >
             Claim
           </Button>
         )}
 
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="secondary" prefix={<EllipsisIcon />} />
-          </DropdownMenuTrigger>
-
-          <DropdownMenuContent side="bottom" align="end" className="min-w-36">
-            <DropdownMenuItem asChild>
-              <button
-                type="button"
-                className={navLinkVariants()}
-                onClick={() => setIsReportOpen(true)}
-              >
-                <TriangleAlertIcon className="shrink-0 size-4 opacity-75" />
-                Report
-              </button>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Tooltip tooltip="Send a report/suggestion">
+          <Button
+            size="md"
+            variant="secondary"
+            prefix={<TriangleAlertIcon />}
+            onClick={() => setIsReportOpen(true)}
+            aria-label="Report"
+          />
+        </Tooltip>
 
         {children}
+
+        <ToolReportDialog tool={tool} isOpen={isReportOpen} setIsOpen={setIsReportOpen} />
+
+        {!tool.owner && (
+          <ToolClaimDialog tool={tool} isOpen={isClaimOpen} setIsOpen={setIsClaimOpen} />
+        )}
       </Stack>
-
-      <ToolReportDialog tool={tool} isOpen={isReportOpen} setIsOpen={setIsReportOpen} />
-
-      {!tool.owner && (
-        <ToolClaimDialog tool={tool} isOpen={isClaimOpen} setIsOpen={setIsClaimOpen} />
-      )}
     </TooltipProvider>
   )
 }
