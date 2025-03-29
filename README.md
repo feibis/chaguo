854ac1523c73a7572faf6dce517017ca9caeaff3
Piotr Kulpinski <piotr@kulpinski.pl>
2025-03-29 12:10:19 +0100

    update logo and readme file

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# DirStarter
+![Dirstarter](https://dirstarter.com/opengraph.png)
 
-Complete documentation coming soon!
+# DirStarter - Next.js Directory Boilerplate
+
+DirStarter is a complete Next.js boilerplate for building profitable directory websites. Make money from day one with our battle-tested stack and built-in monetization features.
+
+## Author
+
+This project is created by [Piotr Kulpinski](https://kulpinski.dev), the founder of [OpenAlternative](https://openalternative.co), [DevSuite](https://devsuite.co) and other directory websites. This boilerplate is the fruit of years of trial and error, and I'm sharing it with you to save you the time and effort it took me to build it.
+
+If you are interested in building profitable directory websites, please follow me on X: [@piotrkulpinski](https://x.com/piotrkulpinski) or BlueSky: [@kulpinski.dev](https://bsky.app/profile/kulpinski.dev).
+
+## Documentation
+
+The documentation is available on the [website](https://dirstarter.com/docs). It includes guides, tutorials, and detailed explanations of the code. I designed it to be as beginner-friendly as possible, so you can start making money from day one.
+
+If you found anything that could be improved, please let me know or contribute to the documentation yourself.
+
+## Third-Party Services
+
+Dirstarter uses the following third-party services:
+
+- Database: [Postgres](https://postgres.org)
+- Analytics: [Plausible](https://plausible.io), [PostHog](https://posthog.com)
+- Newsletter: [Beehiiv](https://kulp.in/beehiiv)
+- Background Jobs: [Inngest](https://inngest.com)
+- File Storage: [AWS S3](https://aws.amazon.com/s3)
+- Payments: [Stripe](https://stripe.com)
+- Screenshots: [ScreenshotOne](https://kulp.in/screenshotone)
+
+Make sure to set up accounts with these services and have the credentials ready.
+
+## License
+
+For any details on the license, please refer to the [License Policy](https://dirstarter.com/license-policy) page.
--- components/web/footer.tsx
+++ components/web/footer.tsx
@@ -9,6 +9,7 @@ import { Stack } from "~/components/common/stack"
 import { Tooltip } from "~/components/common/tooltip"
 import { ExternalLink } from "~/components/web/external-link"
 import { NewsletterForm } from "~/components/web/newsletter-form"
+import { LogoSymbol } from "~/components/web/ui/logo-symbol"
 import { NavLink } from "~/components/web/ui/nav-link"
 import { config } from "~/config"
 import { cx } from "~/utils/cva"
@@ -37,8 +38,8 @@ export const Footer = ({ children, className, hideNewsletter, ...props }: Footer
             </H5>
 
             <p className="-mt-2 px-0.5 text-sm text-muted-foreground first:mt-0">
-              Join {formatNumber(5000, "standard")}+ other members and get updates on new directory
-              websites.
+              Join {formatNumber(5000, "standard")}+ other members and get updates straight to your
+              inbox.
             </p>
 
             <NewsletterForm medium="footer_form" />
@@ -101,16 +102,7 @@ export const Footer = ({ children, className, hideNewsletter, ...props }: Footer
           className="contents font-medium text-foreground hover:text-secondary-foreground"
           doFollow
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 216 251"
-            role="img"
-            aria-label="Dirstarter Logo"
-            className="h-4 w-auto fill-current"
-          >
-            <path d="M0 105v93c0 3.18.29 6.35 2.53 8.6l42.25 42.24C48.56 252.62 55 250.34 55 245v-93c0-3.19-1.05-6.36-3.31-8.62l-42.6-42.6C5.28 96.97-.02 99.64 0 105ZM74 56v132c0 3.18.79 6.14 3.03 8.38l42.25 42.24c3.77 3.78 10.72.73 10.72-4.62V103c0-3.18-1.55-6.5-3.8-8.76L83.56 51.62C79.8 47.84 74 50.66 74 56ZM148 6v155c0 3.18.84 6.6 3.08 8.85l54.99 54.98c3.78 3.78 9.93 1.02 9.93-4.33v-155c0-3.18-.76-6.1-3.01-8.36L157.62 1.77C153.84-2 148 .66 148 6Z" />
-            <path d="M74 56v132c0 3.18.79 6.14 3.03 8.38l42.25 42.24c3.77 3.78 10.72.73 10.72-4.62V103c0-3.18-1.55-6.5-3.8-8.76L83.56 51.62C79.8 47.84 74 50.66 74 56Z" />
-          </svg>
+          <LogoSymbol className="h-4 w-auto fill-current" />
           Dirstarter
         </ExternalLink>
       </Stack>
